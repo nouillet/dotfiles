817bdd006a28702085623835fb636c0485334b4b
Kevin Pelletier <kev.pelletier@icloud.com>
2022-07-20 20:08:40 -0400

    Function linkConfigurationFiles done!

--- install.sh
+++ install.sh
@@ -1,20 +1,63 @@
 #!/bin/bash
 
 # Author: Kevin "nouillet" Pelletier
-# License?
 # 
 # 
 # Just a little script to configure symlinks in your home folder based on the files
-# from this repo. 
+# from this repo. Make sure to run this script from the repository's top level
 
 
-for file in `ls -p | grep -v /` ; do
-    echo "Creating $file symlink into $HOME/.$file\n"
-    ln -s ./$file $HOME/.$file
+# Global variables
+cwd=`pwd`
+backup_folder="$HOME/.nouillet_backup"
 
-echo "Symlinks done!\n"
-echo "Copying vim plugins configuration files"
+# Create backup folder
+mkdir $backup_folder
 
-for file in `ls ./vim/plugins/` ; do
+link_configuration_files() {
+    
+    for file in `ls -p | grep -v / | grep -v install.sh | grep -v tags` ; do
+        # TODO
+        #   - If target exists and is a file, back them up. 
+        #   - If target exists validate if it comes from here. - DONE
+        #   - If target exists and not from this repo, remove link and create a new one - DONE
+        
+        if [ -L "$HOME/.$file" ] ; then
+            link=$(readlink -f "$HOME/.$file")
+            if [ $link = "$cwd/$file" ] ; then
+                echo "Symlink $HOME/.$file already exists"
+            else
+                rm "$HOME/.$file"
+            fi
+        elif [ -f "$HOME/.$file" ] ; then
+            echo "Backing up file "$HOME/.$file" in "$backup_folder""
+            mv "$HOME/.$file" "$backup_folder/"
+        else
+            echo "Creating $file symlink into $HOME/.$file"
+            ln -s $cwd/$file $HOME/.$file
+        fi
+    done
+    
+    echo "Symlinks done!"
+    
+}
+
+link_vim_plugins_configurations() {
+    
+    if [ ! -e "$HOME/.config" ] ; then
+        mkdir $HOME/.config
+    fi
+
+    if [ -e "$HOME/.config/nvim" ] ; then
+        echo "Destination folder $HOME/.config/nvim already exists. Backing it up."
+        mv "$HOME/.config/nvim" "$backup_folder/.config"
+    fi
+
+    # TODO
+    # If folder already exists, move to backup
+    ln -s $cwd/config/vim $HOME/.config/vim
+}
 
 
+link_configuration_files
+# link_vim_plugins_configurations
