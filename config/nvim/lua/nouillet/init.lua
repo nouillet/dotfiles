05456f7a150d190158bf1309bda311c0a03268b0
nouillet <kev.pelletier@icloud.com>
2022-09-28 08:38:36 -0400

    Added lsp setups

--- config/nvim/lua/nouillet/init.lua
+++ config/nvim/lua/nouillet/init.lua
@@ -1,2 +1,17 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
+
+require'lspconfig'.ansiblels.setup{
+    settings = {
+        ansible = {
+            ansibleLint = {
+                enabled = false
+            },
+            python = {
+                interpreterPath = "/home/kpelletier/Projects/CI/Ansible/.venv/bin/python"
+            }
+        }
+    }
+}
+
+require'lspconfig'.jdtls.setup{}
