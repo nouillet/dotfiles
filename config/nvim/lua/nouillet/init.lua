8a422bcbe86a418f44226d03eaf3b28754eb7bc1
nouillet <kev.pelletier@icloud.com>
2022-10-28 10:49:25 -0400

    Updated coc settings

--- config/nvim/lua/nouillet/init.lua
+++ config/nvim/lua/nouillet/init.lua
@@ -1,17 +1,17 @@
-require("mason").setup()
-require("mason-lspconfig").setup()
+-- require("mason").setup()
+-- require("mason-lspconfig").setup()
 
-require'lspconfig'.ansiblels.setup{
-    settings = {
-        ansible = {
-            ansibleLint = {
-                enabled = false
-            },
-            python = {
-                interpreterPath = "/home/kpelletier/Projects/CI/Ansible/.venv/bin/python"
-            }
-        }
-    }
-}
+-- require'lspconfig'.ansiblels.setup{
+--     settings = {
+--         ansible = {
+--             ansibleLint = {
+--                 enabled = false
+--             },
+--             python = {
+--                 interpreterPath = "/home/kpelletier/Projects/CI/Ansible/.venv/bin/python"
+--             }
+--         }
+--     }
+-- }
 
-require'lspconfig'.jdtls.setup{}
+-- require'lspconfig'.jdtls.setup{}
