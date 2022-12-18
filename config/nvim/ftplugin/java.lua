39ef24a64282d1e3260a4c436bef5e4b5a00ea9c
Kevin Pelletier <kev.pelletier@icloud.com>
2022-12-18 13:55:25 -0500

    jdtls now loaded via ftplugin

--- config/nvim/ftplugin/java.lua
+++ config/nvim/ftplugin/java.lua
@@ -2,7 +2,7 @@ local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 
 local config = {
     cmd = {
-      '/home/kpelletier/.local/jdks/amazon-corretto-17/bin/java',
+      '/home/nouillet/.sdkman/candidates/java/17.0.5-amzn/bin/java',
       '-Declipse.application=org.eclipse.jdt.ls.core.id1',
       '-Dosgi.bundles.defaultStartLevel=4',
       '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -12,9 +12,9 @@ local config = {
       '--add-modules=ALL-SYSTEM',
       '--add-opens', 'java.base/java.util=ALL-UNNAMED',
       '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-      '-jar' ,'/home/kpelletier/GitHub/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-      '-configuration', '/home/kpelletier/GitHub/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/config_linux',
-      '-data', '/home/kpelletier/.local/share/jdtls/' .. project_name
+      '-jar' ,'/home/nouillet/Projects/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
+      '-configuration', '/home/nouillet/Projects/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/config_linux',
+      '-data', '/home/nouillet/.local/share/jdtls/' .. project_name
     },
 
 
@@ -24,11 +24,11 @@ local config = {
           runtimes = {
             {
               name = "JavaSE-11",
-              path = "/home/kpelletier/.local/jdks/amazon-corretto-11.0.12.7.1-linux-x64/"
+              path = "/home/nouillet/.sdkman/candidates/java/11.0.17-amzn/"
             },
             {
               name = "JavaSE-1.8",
-              path = "/home/kpelletier/.local/jdks/jdk1.8.0_192/"
+              path = "/home/nouillet/.sdkman/candidates/java/8.0.352-amzn/"
             }
           }
         }
