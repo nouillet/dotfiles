73520d420a6aeedb3f8a048e21b5df0d3bc04d96
Kevin Pelletier <kev.pelletier@icloud.com>
2023-08-15 12:51:46 -0400

    Updated nvim-tree and telescope plugins settings

--- config/nvim/lua/plugins/nvim-telescope.lua
+++ config/nvim/lua/plugins/nvim-telescope.lua
@@ -1,18 +1,30 @@
+local actions = require("telescope.actions")
+
 require('telescope').setup {
-  defaults = {},
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-u>"] = false,
+        ["<esc>"] = actions.close,
+      },
+    },
+  },
   pickers = {
-    buffers = {
-      theme = "ivy"
-    },
-    find_files = {
-      theme = "ivy"
-    },
-    git_files = {
-      theme = "ivy"
-    },
-    live_grep = {
-      theme = "ivy"
-    }
+    -- buffers = {
+    --   theme = "ivy"
+    -- },
+    -- find_files = {
+    --   theme = "ivy"
+    -- },
+    -- git_files = {
+    --   theme = "ivy"
+    -- },
+    -- live_grep = {
+    --   theme = "ivy"
+    -- }
   }
 }
+
+require("telescope").load_extension('harpoon')
+require("telescope").load_extension('git_worktree')
 
