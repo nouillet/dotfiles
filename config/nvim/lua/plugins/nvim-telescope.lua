2fef075f00e6202127dee623582d321d722d4d34
Kevin Pelletier <kev.pelletier@icloud.com>
2024-08-04 16:31:09 -0400

    feat(i3): Updated look and plugins

--- config/nvim/lua/plugins/nvim-telescope.lua
+++ config/nvim/lua/plugins/nvim-telescope.lua
@@ -17,22 +17,7 @@ return {
           },
         },
       },
-      pickers = {
-        -- buffers = {
-        --   theme = "ivy"
-        -- },
-        -- find_files = {
-        --   theme = "ivy"
-        -- },
-        -- git_files = {
-        --   theme = "ivy"
-        -- },
-        -- live_grep = {
-        --   theme = "ivy"
-        -- }
-      }
     }
-
 
     require("telescope").load_extension('harpoon')
     -- require("telescope").load_extension('git_worktree')
